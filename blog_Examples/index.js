e32cfe1f4247e255d6bd3333ea0cf5f7eedcfa86
shubh ujala <shubhujala.code@gmail.com>
2026-03-21 18:16:00 +0530

    blog_examples

---
+++ blog_Examples/index.js
@@ -0,0 +1,167 @@
+// const arr = ["chai","coffee","cold-drink"];
+// // unshift
+// console.log('unshifted the coke: ');
+// arr.unshift("coke");
+// console.log(arr);
+
+// //shift
+// console.log("\n\n");
+
+// console.log("shift the coke");
+// arr.shift();
+// console.log(arr);
+
+// console.log("\n");
+
+// const arr = [1,2,3,4,5,6];
+
+// const result = arr.map((e)=> e*10);
+
+// console.log(`Original Array : [${arr}]`);
+
+// console.log(`New Array : [${result}]`);
+
+// console.log("\n");
+
+// console.log("\n\n\n\n\n\n\n\n\n\n\n\n");
+
+
+
+
+// const myObj = {
+//   value: 10,
+// };
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// const result = arr.map(function(e){
+//     return e * this.value;
+// },myObj)
+
+// console.log(result);
+
+
+
+
+
+// const arr = [1,2,3,4,5,6];
+
+// arr.forEach((e)=> console.log(e*2));
+
+
+
+// // 
+// const arr = ["shubh",1,2,"work",true,{taks:"gym"}];
+
+// arr.forEach((e)=> console.log(e));
+
+
+// console.log(arr.toReversed());
+
+
+
+
+
+// let a = 10;
+// let b = 20;
+
+// let c = a + b;
+
+// console.log(c);
+
+// let x = 70;
+// let y = 80;
+
+// let z = x + y;
+// console.log(z);
+
+
+// function add(x, y){
+//     return x + y;
+// }
+
+// let ans1 = add(4,5);
+// console.log(ans1);
+
+
+
+// const add = function(x,y){
+//     return x + y;
+// }
+
+// let ans = add(10,20);
+// console.log(ans);
+
+
+
+
+
+// const ans = addNums(2,3,4);
+// console.log(ans);
+
+function addNums(x,y,z){
+    return x + y + z;
+}
+
+
+
+
+// const myDetails = ["shubh", 22, "jalandhar"];
+
+
+// const myDetails = {
+//     name : "Shubh",
+//     age : 22,
+//     city : "jalandhar",
+//     salary : '10Lpa'
+// }
+
+// for (const key in myDetails) {
+//     console.log(myDetails[key]);
+// }
+
+
+
+
+// let b = 10;
+// {
+//     let a = 20;
+// }
+
+
+var name = "shubh"
+
+
+// console.log(a);
+
+
+// let a = 20;
+
+// {
+//     let z = "block scope"
+//     console.log(z);
+// }
+// function sayHi(){
+//     let name = "shubh";
+//     console.log(name);
+    
+// }
+
+// console.log(name);
+
+
+
+let marks = [90,33, 99, 100];
+
+
+
+// var a = 30;
+// function func(){
+//     var a = 50;
+// }
+
+
+// let name = "shubh"
+// let $name = "shubh";
+// let _db = "mydb"
+// let $_items = "my items";
