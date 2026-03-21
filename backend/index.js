b466650db01aa4c7302af44e9758e19bde7348b2
shubh ujala <shubhujala.code@gmail.com>
2026-03-21 18:23:39 +0530

    backend basics

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+   if( req.method === 'GET' && req.url === '/menu'){
+        res.writeHead(200,{'content-type':'application/json'});
+        res.end(JSON.stringify({
+            items:['thali','biryani']
+        }))
+   }else if(req.method === 'POST' && req.url === '/order'){
+    let data = '';
+    req.on('data', chnk => data+=chnk);
+    req.on('end',()=>{
+        const order = JSON.parse(data);
+        res.writeHead(200,{'content-type':'application/json'});
+        res.end(JSON.stringify({
+            status:'recieved',
+            order
+        }))
+    })
+   }
+
+})
+
+server.listen(3000);
